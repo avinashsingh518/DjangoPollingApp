701922249162110bf59ba840518c21a834c1c5fa
Avinash Kumar Singh <90401157+avinashsingh518@users.noreply.github.com>
2024-09-29 01:36:35 +0530

    Delete DjangoPollingApp/djangoPollingApp directory

--- DjangoPollingApp/djangoPollingApp/pollapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import *
-from rest_framework import serializers
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Register
-        fields=['emailid', 'firstname', 'lastname','username','mobile','password']
